b82e3fdb0a2a6f4c2291fbcbce0ede29846fcc98
havebear <616749285@qq.com>
2021-05-19 17:48:09 +0800

    chore: 更新

---
+++ src/views/workbench/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: 616749285@qq.com
+ * @Date: 2021-05-19 14:54:01
+ * @LastEditors: 616749285@qq.com
+ * @LastEditTime: 2021-05-19 15:12:57
+ * @Description:  工作台
+-->
+
+<template>
+  <div>工作台</div>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
